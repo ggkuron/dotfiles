59e4d0e224f5cfaccde3dd393a83c7a5b309bec9
ggkuron <ggkuron@gmail.com>
2013-10-03 00:36:41 +0900

    add gnome-terminal config files

--- setup_common.sh
+++ setup_common.sh
@@ -3,6 +3,7 @@
 mkdir_if_needed() {
     if [ $# -ne 1 ] || [ ! -d $PWD/$1 ]; then
         echo "You or I am wrong.." 1>&2
+        echo "$PWD/$1"
         exit 1
     fi
     if [ ! -d $HOME/$1 ]; then
@@ -18,7 +19,7 @@ mkdir_if_needed() {
 }
 
 sym_to_home() {
-    if [ $# -ne 1 ] || [ ! -f $PWD/$1 ]; then
+    if [ $# -ne 1 ] || [ ! -e $PWD/$1 ]; then
         echo "file $1 does not exist. exit script .. " 1>&2
         exit 1
     elif [ -e $HOME/$1 ]; then
