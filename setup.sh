aeee489801eba4f48be695ab0d39c0375530ed13
ggkuron <ggkuron@gmail.com>
2013-10-16 21:17:54 +0900

    fix up xmodmap files

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ sym_to_home .zshrc
 sym_to_home .vimrc
 sym_to_home .gvimrc
 sym_to_home .tmux.conf
+sym_to_home .xmodmap
+sym_to_home .caps_killer
 
 if mkdir_if_needed .config/Terminal ; then
     sym_to_home .config/Terminal/terminalrc
